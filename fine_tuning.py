714a78a8fa8388f5e432605d64227a04d330ab39
Iztihad Mustakim <iztihad.cero@gmail.com>
2026-04-27 15:43:49 +0600

    Tiny ViT was fine-tuned and the best weights were saved

---
+++ fine_tuning.py
@@ -0,0 +1,23 @@
+def fine_tune(model, model_name, state):
+
+    
+
+    if(state == "partial"):
+        if(model_name == "mobilenet_v2"):
+            for param in model.features[-3:].parameters():
+                param.requires_grad = True
+        elif(model_name == "resnet_50"):
+            for param in model.layer4.parameters():
+                param.requires_grad = True
+        elif(model_name == "efficientnet_v2_s"):
+            for param in model.features[-2:].parameters():
+                param.requires_grad = True
+        elif(model_name == "tiny_vit"):
+            for param in model.stages[-1].parameters():
+                param.requires_grad = True
+
+    elif(state == "full"):
+        for params in model.parameters():
+            params.requires_grad = True
+
+    return model
